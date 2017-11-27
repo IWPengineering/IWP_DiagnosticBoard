d441973d5d9c7a088bf0bec5b4c568d6887177fe
Randall Fish <rfish@messiah.edu>
2017-11-27 16:50:06 -0500

    Added Watchdog Timer

--- IWP_DiagnosticBoard.X/Main.c
+++ IWP_DiagnosticBoard.X/Main.c
@@ -8,6 +8,25 @@
 
 #include <xc.h>
 
+
+
+/*********************************************************************
+ * Function: ClearWatchDogTimer()
+ * Input: none
+ * Output: none
+ * Overview: You can use just ClrWdt() as a command.  However, I read in a forum 
+ *           http://www.microchip.com/forums/m122062.aspx
+ *           that since ClrWdt() expands to an asm command, the presence of the 
+  *          asm will stop the compiler from optimizing any routine that it is a 
+  *          part of.  Since I want to call this in Main, that would be a problem
+ * Note: Library
+ * TestDate: 1-2-2017
+ ********************************************************************/
+ void ClearWatchDogTimer(void){
+     ClrWdt();
+ }
+
 void main(void) {
+    ClearWatchDogTime();
     return;
 }
