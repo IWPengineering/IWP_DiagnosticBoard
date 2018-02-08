f1cf5a4b4e2a9850876c9b157fc7ffc6c6ad8eb2
Randall Fish <rfish@messiah.edu>
2018-02-08 11:32:17 -0500

    Pragma Updated Timer Issues
    Changed PIC to low power version
    Changed PRAGMA
    made Timer1 a 500khz clock and updated delayMS

--- IWP_DiagnosticBoard.X/Pin_Manager.c
+++ IWP_DiagnosticBoard.X/Pin_Manager.c
@@ -17,246 +17,248 @@
 /////////////////////////////////////////////////////////////////////
 void pinDirectionIO(int pin, int io) { // 1 is an input, 0 is an output
 	// Pin 1 can't change direction
-    if (pin == 2) {
-        TRISAbits.TRISA0 = io;
-    }
-    else if (pin == 3) {
-        TRISAbits.TRISA1 = io;
-    }
-    else if (pin == 4) {
-        TRISAbits.TRISA2 = io;
-    }
-    else if (pin == 5) { // WPS_OUT
-        TRISAbits.TRISA3 = io;
-    }
-    else if (pin == 6) { // ADXL_335_X
-        TRISAbits.TRISA4 = io;
-    }
-    else if (pin == 7) { // ADXL_335_Y
-        TRISAbits.TRISA5 = io; 
-    }
-    // Pin8 -  VSS - do nothing
-    else if (pin == 9) {
-        TRISAbits.TRISA7 = io;
-    }
-    else if (pin == 10) { // Push button 
-        TRISAbits.TRISA6 = io;
-    }
-    else if (pin == 11) { // EEPROM (CS with bar) NEED TO CHECK
-        TRISCbits.TRISC0 = io;
-    }
-    else if (pin == 12) { // PIC_PIN4
-        TRISCbits.TRISC1 = io;
-    }
-    else if (pin == 13) { // PIC_PIN5
-        TRISCbits.TRISC2 = io;
-    }
-    else if (pin == 14) { // EEPROM SCK NEED TO CHECK
-        TRISCbits.TRISC3 = io; 
-    }
-    else if (pin == 15) { // EEPROM SO NEED TO CHECK 
-        TRISCbits.TRISC4 = io;
-    }
-    else if (pin == 16) { // EEPROM SI NEED TO CHECK 
-        TRISCbits.TRISC5 = io;
-    }
-    else if (pin == 17) { // TX (input from control board)
-        TRISCbits.TRISC6 = io; 
-    }
-    else if (pin == 18) { // RX (output to control board)
-        TRISCbits.TRISC7 = io;
-    }
-    // Pin 19 VSS
-    // Pin 20 VDD
-    else if (pin == 21) { // PICKIT_5
-        TRISBbits.TRISB0 = io;
-    }
-    else if (pin == 22) { // PICKIT_4
-        TRISBbits.TRISB1 = io;
-    }
-    else if (pin == 23) { // PIC_PIN12
-        TRISBbits.TRISB2 = io; 
-    }
-    else if (pin == 24) { // LED4
-        TRISBbits.TRISB3 = io;
-    }
-    else if (pin == 25) { // LED5
-        TRISBbits.TRISB4 = io;
-    }
-    else if (pin == 26) { // LED1
-        TRISBbits.TRISB5 = io;
-    }
-    else if (pin == 27) { // LED2
-        TRISBbits.TRISB6 = io;
-    }
-    else if (pin == 28) { // LED3
-        TRISBbits.TRISB7 = io;
-    }
-
+//    if (pin == 2) {
+//        TRISAbits.TRISA0 = io;
+//    }
+//    else if (pin == 3) {
+//        TRISAbits.TRISA1 = io;
+//    }
+//    else if (pin == 4) {
+//        TRISAbits.TRISA2 = io;
+//    }
+//    else if (pin == 5) { // WPS_OUT
+//        TRISAbits.TRISA3 = io;
+//    }
+//    else if (pin == 6) { // ADXL_335_X
+//        TRISAbits.TRISA4 = io;
+//    }
+//    else if (pin == 7) { // ADXL_335_Y
+//        TRISAbits.TRISA5 = io; 
+//    }
+//    // Pin8 -  VSS - do nothing
+//    else if (pin == 9) {
+//        TRISAbits.TRISA7 = io;
+//    }
+//    else if (pin == 10) { // Push button 
+//        TRISAbits.TRISA6 = io;
+//    }
+//    else if (pin == 11) { // EEPROM (CS with bar) NEED TO CHECK
+//        TRISCbits.TRISC0 = io;
+//    }
+//    else if (pin == 12) { // PIC_PIN4
+//        TRISCbits.TRISC1 = io;
+//    }
+//    else if (pin == 13) { // PIC_PIN5
+//        TRISCbits.TRISC2 = io;
+//    }
+//    else if (pin == 14) { // EEPROM SCK NEED TO CHECK
+//        TRISCbits.TRISC3 = io; 
+//    }
+//    else if (pin == 15) { // EEPROM SO NEED TO CHECK 
+//        TRISCbits.TRISC4 = io;
+//    }
+//    else if (pin == 16) { // EEPROM SI NEED TO CHECK 
+//        TRISCbits.TRISC5 = io;
+//    }
+//    else if (pin == 17) { // TX (input from control board)
+//        TRISCbits.TRISC6 = io; 
+//    }
+//    else if (pin == 18) { // RX (output to control board)
+//        TRISCbits.TRISC7 = io;
+//    }
+//    // Pin 19 VSS
+//    // Pin 20 VDD
+//    else if (pin == 21) { // PICKIT_5
+//        TRISBbits.TRISB0 = io;
+//    }
+//    else if (pin == 22) { // PICKIT_4
+//        TRISBbits.TRISB1 = io;
+//    }
+//    else if (pin == 23) { // PIC_PIN12
+//        TRISBbits.TRISB2 = io; 
+//    }
+//    else if (pin == 24) { // LED4
+//        TRISBbits.TRISB3 = io;
+//    }
+//    else if (pin == 25) { // LED5
+//        TRISBbits.TRISB4 = io;
+//    }
+//    else if (pin == 26) { // LED1
+//        TRISBbits.TRISB5 = io;
+//    }
+//    else if (pin == 27) { // LED2
+//        TRISBbits.TRISB6 = io;
+//    }
+//    else if (pin == 28) { // LED3
+//        TRISBbits.TRISB7 = io;
+//    }
+// NEED TO FIX CHECK DATASHEET 
 }
 
 void digitalPinSet(int pin, int set) // 1 for high, 0 for low
 {
-    // Pin 1 can't change direction
-    if (pin == 2) {
-        TRISAbits.TRISA0 = set;
-    }
-    else if (pin == 3) {
-        TRISAbits.TRISA1 = set;
-    }
-    else if (pin == 4) {
-        TRISAbits.TRISA2 = set;
-    }
-    else if (pin == 5) { // WPS_OUT
-        TRISAbits.TRISA3 = set;
-    }
-    else if (pin == 6) { // ADXL_335_X
-        TRISAbits.TRISA4 = set;
-    }
-    else if (pin == 7) { // ADXL_335_Y
-        TRISAbits.TRISA5 = set; 
-    }
-    // Pin8 -  VSS - do nothing
-    else if (pin == 9) {
-        TRISAbits.TRISA7 = set;
-    }
-    else if (pin == 10) { // Push button 
-        TRISAbits.TRISA6 = set;
-    }
-    else if (pin == 11) { // EEPROM (CS with bar) NEED TO CHECK
-        TRISCbits.TRISC0 = set;
-    }
-    else if (pin == 12) { // PIC_PIN4
-        TRISCbits.TRISC1 = set;
-    }
-    else if (pin == 13) { // PIC_PIN5
-        TRISCbits.TRISC2 = set;
-    }
-    else if (pin == 14) { // EEPROM SCK NEED TO CHECK
-        TRISCbits.TRISC3 = set; 
-    }
-    else if (pin == 15) { // EEPROM SO NEED TO CHECK 
-        TRISCbits.TRISC4 = set;
-    }
-    else if (pin == 16) { // EEPROM SI NEED TO CHECK 
-        TRISCbits.TRISC5 = set;
-    }
-    else if (pin == 17) { // TX (input from control board)
-        TRISCbits.TRISC6 = set; 
-    }
-    else if (pin == 18) { // RX (output to control board)
-        TRISCbits.TRISC7 = set;
-    }
-    // Pin 19 VSS
-    // Pin 20 VDD
-    else if (pin == 21) { // PICKIT_5
-        TRISBbits.TRISB0 = set;
-    }
-    else if (pin == 22) { // PICKIT_4
-        TRISBbits.TRISB1 = set;
-    }
-    else if (pin == 23) { // PIC_PIN12
-        TRISBbits.TRISB2 = set; 
-    }
-    else if (pin == 24) { // LED4
-        TRISBbits.TRISB3 = set;
-    }
-    else if (pin == 25) { // LED5
-        TRISBbits.TRISB4 = set;
-    }
-    else if (pin == 26) { // LED1
-        TRISBbits.TRISB5 = set;
-    }
-    else if (pin == 27) { // LED2
-        TRISBbits.TRISB6 = set;
-    }
-    else if (pin == 28) { // LED3
-        TRISBbits.TRISB7 = set;
-    }
-
+//    // Pin 1 can't change direction
+//    if (pin == 2) {
+//        TRISAbits.TRISA0 = set;
+//    }
+//    else if (pin == 3) {
+//        TRISAbits.TRISA1 = set;
+//    }
+//    else if (pin == 4) {
+//        TRISAbits.TRISA2 = set;
+//    }
+//    else if (pin == 5) { // WPS_OUT
+//        TRISAbits.TRISA3 = set;
+//    }
+//    else if (pin == 6) { // ADXL_335_X
+//        TRISAbits.TRISA4 = set;
+//    }
+//    else if (pin == 7) { // ADXL_335_Y
+//        TRISAbits.TRISA5 = set; 
+//    }
+//    // Pin8 -  VSS - do nothing
+//    else if (pin == 9) {
+//        TRISAbits.TRISA7 = set;
+//    }
+//    else if (pin == 10) { // Push button 
+//        TRISAbits.TRISA6 = set;
+//    }
+//    else if (pin == 11) { // EEPROM (CS with bar) NEED TO CHECK
+//        TRISCbits.TRISC0 = set;
+//    }
+//    else if (pin == 12) { // PIC_PIN4
+//        TRISCbits.TRISC1 = set;
+//    }
+//    else if (pin == 13) { // PIC_PIN5
+//        TRISCbits.TRISC2 = set;
+//    }
+//    else if (pin == 14) { // EEPROM SCK NEED TO CHECK
+//        TRISCbits.TRISC3 = set; 
+//    }
+//    else if (pin == 15) { // EEPROM SO NEED TO CHECK 
+//        TRISCbits.TRISC4 = set;
+//    }
+//    else if (pin == 16) { // EEPROM SI NEED TO CHECK 
+//        TRISCbits.TRISC5 = set;
+//    }
+//    else if (pin == 17) { // TX (input from control board)
+//        TRISCbits.TRISC6 = set; 
+//    }
+//    else if (pin == 18) { // RX (output to control board)
+//        TRISCbits.TRISC7 = set;
+//    }
+//    // Pin 19 VSS
+//    // Pin 20 VDD
+//    else if (pin == 21) { // PICKIT_5
+//        TRISBbits.TRISB0 = set;
+//    }
+//    else if (pin == 22) { // PICKIT_4
+//        TRISBbits.TRISB1 = set;
+//    }
+//    else if (pin == 23) { // PIC_PIN12
+//        TRISBbits.TRISB2 = set; 
+//    }
+//    else if (pin == 24) { // LED4
+//        TRISBbits.TRISB3 = set;
+//    }
+//    else if (pin == 25) { // LED5
+//        TRISBbits.TRISB4 = set;
+//    }
+//    else if (pin == 26) { // LED1
+//        TRISBbits.TRISB5 = set;
+//    }
+//    else if (pin == 27) { // LED2
+//        TRISBbits.TRISB6 = set;
+//    }
+//    else if (pin == 28) { // LED3
+//        TRISBbits.TRISB7 = set;
+//    }
+// NEED TO FIX CHECK DATASHEET
 }
 
 //TODO: Should be based off of the RB values, not the AN
 void specifyAnalogPin(int pin, int analogOrDigital) // analogOrDigital = 1 if analog, 0 is digital
 {
-    if (pin == 5) { // WPS_OUT
-        ANSAbits.ANSA3 = analogOrDigital; 
-    }
-    else if (pin == 6) { // ADXL_335_X
-        ANSAbits.ANSA4 = analogOrDigital;
-    }
-    else if (pin == 7) { // ADXL_335_Y
-        ANSAbits.ANSA5 = analogOrDigital;
-    }
-    else if (pin == 9) {
-        ANSAbits.ANSA7 = analogOrDigital;
-    }
-    else if (pin == 10) { // Push button
-        ANSAbits.ANSA6 = analogOrDigital;
-    }
-//    else if (pin == 11) { // CHECK THIS ON DATASHEET 
-//        ANSCbits.ANSC0 = analogOrDigital;
+//    if (pin == 5) { // WPS_OUT
+//        ANSAbits.ANSA3 = analogOrDigital; 
 //    }
-    else if (pin == 12) { // PIC_PIN4
-        ANSCbits.ANSC1 = analogOrDigital;
-    }
-    else if (pin == 13) { // PIC_PIN5
-        ANSCbits.ANSC2 = analogOrDigital;
-    }
-//    else if (pin == 14) { // CHECK THIS ON DATASHEET
-//        ANSCbits.ANSC3 = analogOrDigital;
+//    else if (pin == 6) { // ADXL_335_X
+//        ANSAbits.ANSA4 = analogOrDigital;
 //    }
-//    else if (pin == 15) { // CHECK THIS ON DATASHEET
-//        ANSCbits.ANSC4 = analogOrDigital;
+//    else if (pin == 7) { // ADXL_335_Y
+//        ANSAbits.ANSA5 = analogOrDigital;
 //    }
-//    else if (pin == 16) { // CHECK THIS ON DATASHEET
-//        ANSCbits.ANSC5 = analogOrDigital;
+//    else if (pin == 9) {
+//        ANSAbits.ANSA7 = analogOrDigital;
 //    }
-    else if (pin == 17) { // TX
-        ANSCbits.ANSC6 = analogOrDigital;
-    }
-    else if (pin == 18) { // RX
-        ANSCbits.ANSC7 = analogOrDigital;
-    }
-    else if (pin == 21) { // PICKIT_5
-        ANSBbits.ANSB0 = analogOrDigital;
-    }
-    else if (pin == 22) { // PICKIT_4
-        ANSBbits.ANSB1 = analogOrDigital;
-    }
-    else if (pin == 23) {
-        ANSBbits.ANSB2 = analogOrDigital;
-    }
-    else if (pin == 24) {
-        ANSBbits.ANSB3 = analogOrDigital;
-    }
-    else if (pin == 25) {
-        ANSBbits.ANSB4 = analogOrDigital;
-    }
-    else if (pin == 26) {
-        ANSBbits.ANSB5 = analogOrDigital;
-    }
-    else if (pin == 27) {
-        ANSBbits.ANSB6 = analogOrDigital;
-    }
-    else if (pin == 28) {
-        ANSBbits.ANSB7 = analogOrDigital;
-    }
+//    else if (pin == 10) { // Push button
+//        ANSAbits.ANSA6 = analogOrDigital;
+//    }
+////    else if (pin == 11) { // CHECK THIS ON DATASHEET 
+////        ANSCbits.ANSC0 = analogOrDigital;
+////    }
+//    else if (pin == 12) { // PIC_PIN4
+//        ANSCbits.ANSC1 = analogOrDigital;
+//    }
+//    else if (pin == 13) { // PIC_PIN5
+//        ANSCbits.ANSC2 = analogOrDigital;
+//    }
+////    else if (pin == 14) { // CHECK THIS ON DATASHEET
+////        ANSCbits.ANSC3 = analogOrDigital;
+////    }
+////    else if (pin == 15) { // CHECK THIS ON DATASHEET
+////        ANSCbits.ANSC4 = analogOrDigital;
+////    }
+////    else if (pin == 16) { // CHECK THIS ON DATASHEET
+////        ANSCbits.ANSC5 = analogOrDigital;
+////    }
+//    else if (pin == 17) { // TX
+//        ANSCbits.ANSC6 = analogOrDigital;
+//    }
+//    else if (pin == 18) { // RX
+//        ANSCbits.ANSC7 = analogOrDigital;
+//    }
+//    else if (pin == 21) { // PICKIT_5
+//        ANSBbits.ANSB0 = analogOrDigital;
+//    }
+//    else if (pin == 22) { // PICKIT_4
+//        ANSBbits.ANSB1 = analogOrDigital;
+//    }
+//    else if (pin == 23) {
+//        ANSBbits.ANSB2 = analogOrDigital;
+//    }
+//    else if (pin == 24) {
+//        ANSBbits.ANSB3 = analogOrDigital;
+//    }
+//    else if (pin == 25) {
+//        ANSBbits.ANSB4 = analogOrDigital;
+//    }
+//    else if (pin == 26) {
+//        ANSBbits.ANSB5 = analogOrDigital;
+//    }
+//    else if (pin == 27) {
+//        ANSBbits.ANSB6 = analogOrDigital;
+//    }
+//    else if (pin == 28) {
+//        ANSBbits.ANSB7 = analogOrDigital;
+//    }
+    // NEED TO FIX CHECK DATASHEET
 
 }
 
 
 void pinSampleSelectRegister(int pin) { //  A/D Sample Select Register (CHECK WHEN USED)  
                         //(this is only used in the readADC() function)
-    if (pin == 6) { // ADXL_335_X NEED TO CHECK
-        AD1CHSbits.CH0SA = 4; // ANA4
-    }
-    else if (pin == 7) {
-        AD1CHSbits.CH0SA = 5; // ANA5
-    }
-    else if (pin == 17) {
-        AD1CHSbits.CH0SA = 6; // ANC6 NEED TO CHECK 
-    }
+//    if (pin == 6) { // ADXL_335_X NEED TO CHECK
+//        AD1CHSbits.CH0SA = 4; // ANA4
+//    }
+//    else if (pin == 7) {
+//        AD1CHSbits.CH0SA = 5; // ANA5
+//    }
+//    else if (pin == 17) {
+//        AD1CHSbits.CH0SA = 6; // ANC6 NEED TO CHECK 
+//    }
+    // NEED TO FIX CHECK DATASHEET 
    
 }
 
